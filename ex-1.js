ce302ca0290586c20c1d74c9814e64cfbb3490c9
Pongsapak <riddiqulus@gmail.com>
2026-06-14 17:49:58 +0700

    Finish javascript boolean and logical operations

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,9 @@
 let isOver18;
 let hasCriminalBlacklist;
-
 let isAllow;
 
 // Start coding here
-
+isOver18 = true;
+hasCriminalBlacklist = false;
+isAllow = isOver18 && !hasCriminalBlacklist;
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -6,7 +6,30 @@ let isWeekday;
 let hasBoughtProductFromITCategory;
 let hasAttendedDiscountEvent;
 let isPlatinum;
-
 let hasPromotion;
 
+// John ซื้อ 4001 บาท
+lastMonthPaidMoreThan4000 = true;
+
+// ซื้อวันศุกร์
+isWeekday = true;
+
+// ไม่เคยซื้อสินค้า IT
+hasBoughtProductFromITCategory = false;
+
+// เคยเข้ากิจกรรมลดราคา
+hasAttendedDiscountEvent = true;
+
+// เป็น Gold ไม่ใช่ Platinum
+isPlatinum = false;
+
+hasPromotion =
+    (
+        lastMonthPaidMoreThan4000 &&
+        isWeekday &&
+        !hasBoughtProductFromITCategory &&
+        !hasAttendedDiscountEvent
+    )
+    ||
+    isPlatinum;
 console.log(hasPromotion);
